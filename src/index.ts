f98d75311d21eec5b74010e2a9ee6ef0f091a187
jamesmccomish <james.mcc94@hotmail.co.uk>
2024-11-01 21:42:45 +0100

    chore: restructure and config ts building

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { erc20Abi, erc20AbiOptional, erc20AbiRequired } from './ercs/erc-20/abi';
+export { erc721AbiRequired, erc721EnumerableAbi, erc721MetadataAbi, erc721TokenReceiverAbi } from './ercs/erc-721/abi.human-readable';
+export { erc165Abi } from './ercs/erc-165/abi.human-readable';
