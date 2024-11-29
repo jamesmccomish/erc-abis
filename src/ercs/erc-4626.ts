6a5e122b8165668cc037d038f539fbf6e69788ef
jamesmccomish <james.mcc94@hotmail.co.uk>
2024-11-29 22:06:07 +0100

    add missing events and erc20 abi to erc4626

--- src/ercs/erc-4626.ts
+++ src/ercs/erc-4626.ts
@@ -1,5 +1,9 @@
+import { erc2612Abi } from "./erc-2612";
+
 /** [ERC-4626: Tokenized Vaults](https://eips.ethereum.org/EIPS/eip-4626) */
 export const erc4626AbiRequired = [
+	"event Deposit(address sender indexed, address owner indexed, uint256 assets, uint256 shares)",
+	"event Withdraw(address sender indexed, address receiver indexed, address owner indexed, uint256 assets, uint256 shares)",
 	"function asset() view returns (address)",
 	"function totalAssets() view returns (uint256)",
 	"function convertToShares(uint256 assets) view returns (uint256)",
@@ -17,3 +21,7 @@ export const erc4626AbiRequired = [
 	"function previewRedeem(uint256 shares) view returns (uint256)",
 	"function redeem(uint256 shares, address receiver, address owner) returns (uint256)",
 ];
+
+// ERC-2612 is optional but generally recommended for ERC-4626, and since it 
+// includes ERC-20 which is required for ERC-4626, we can include it here.
+export const erc4626Abi = [...erc2612Abi, ...erc4626AbiRequired];
